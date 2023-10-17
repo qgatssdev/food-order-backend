f38a7aaf4531f0a697d6bb3d32fa084cb3bc3299
qgatssdev <chivoomodu@gmail.com>
2023-10-17 23:11:24 +0100

    feat: create food route

---
+++ src/models/Food.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Document, Model, Mongoose } from 'mongoose';
+
+interface FoodDoc extends Document {
+  vendorId: string;
+  name: string;
+  description: string;
+  foodType: string;
+  category: string;
+  readyTime: string;
+  price: number;
+  rating: number;
+  images: [string];
+}
+
+const FoodSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    foodType: { type: String, required: true },
+    category: { type: String },
+    readyTime: { type: String, required: true },
+    price: { type: String, required: true },
+    rating: { type: Number},
+    images: { type: [String], required: true },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.__v;
+        delete ret.createdAt, delete ret.updatedAt;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+const Food = mongoose.model<FoodDoc>('food', FoodSchema);
+
+export { Food };
