04ea54a0d34b32cc941f7ef042b4d78e5bbfded9
qgatssdev <chivoomodu@gmail.com>
2023-10-18 23:49:04 +0100

    feat: introduce multer and add new routes

--- src/routes/VendorRoute.ts
+++ src/routes/VendorRoute.ts
@@ -1,23 +1,37 @@
 import express, { Request, Response, NextFunction } from 'express';
 import {
+  AddFood,
+  GetFoods,
   GetVendorProfile,
   UpdateVendorProfile,
   UpdateVendorService,
   VendorLogin,
 } from '../controllers';
 import { Authenticate } from '../middlewares';
+import multer from 'multer';
 
 const router = express.Router();
 
+const imageStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname);
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '_' + file.originalname);
+  },
+});
+
+const images = multer({ storage: imageStorage }).array('images', 10);
+
 router.post('/login', VendorLogin);
 
-router.use(Authenticate)
+router.use(Authenticate);
 router.get('/profile', GetVendorProfile);
 router.patch('/profile', UpdateVendorProfile);
 router.patch('/service', UpdateVendorService);
 
-router.post('/food')
-router.get('/foods')
+router.post('/food', images, AddFood);
+router.get('/foods', GetFoods);
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.json({
